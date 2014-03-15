77d642bd3b510ddb49cdc5283bd88ec68f6e07f4
Chase Southard <chase.southard@gmail.com>
2014-03-14 22:58:16 -0400

    user secrets

--- app.rb
+++ app.rb
@@ -1,15 +1,23 @@
 require 'sinatra'
 
-SECRET_KEY="26289216d0613f8978b7d2cf64f27914"
+users = {
+  todd: 'bbc2ef86a33282c490c90714c5ae09a0',
+  nick: 'bbc24fa7982b62ee333d519082ff47f1',
+  steev: '9b7d14fdc931d6e0c3f801e948016819',
+  sarah: '797c191a2c25d41bf5c6ba516c6a485d',
+  mike: 'c2d9c4e37e06037c3051c31772de6aa3',
+  chase: '8d181bfc818b3bf452b9f32d38390d43',
+  chaserx: 'a4713062efaeb3bd0241cd6d1f56ce78'
+}
 
 get '/say' do
-  if params && words=params[:words] && params[:secret] == SECRET_KEY
+  if params && words=params[:words] && users.has_value?(params[:secret])
     `say #{words}`
   end
 end
 
 post '/say' do
-  if params && words=params[:words] && params[:secret] == SECRET_KEY
+  if params && words=params[:words] && users.has_value?(params[:secret])
     `say #{words}`
   end
 end
