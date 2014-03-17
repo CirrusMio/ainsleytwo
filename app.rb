6e85d891770b1685948c766e704c57e958f903c4
nuex <nx@nu-ex.com>
2014-03-17 16:21:32 -0400

    fix key spitting

--- app.rb
+++ app.rb
@@ -19,12 +19,13 @@ class AinsleyTwo < Sinatra::Base
   # Return a key if the incoming request is from an internal network
   # Save the key to the whitelist
   get '/key' do
-    if subnet = config['subnet'].first
-      internal = IPAddress.parse(subnet)
-      incoming = IPAddress.parse(request.ip)
+    if subnet = config['subnet']
+      internal = IPAddr.new(subnet)
+      incoming = IPAddr.new(request.ip)
       if internal.include?(incoming)
         key = SecureRandom.hex
-        config['whitelist_keys'].push(key)
+        whitelist = config['whitelist_keys'] ||= []
+        whitelist.push(key)
         File.open('config.yml', 'w') {|f| f.write(config.to_yaml) }
         key
       end
