4e742b74586f772776fb99b8d6f068de6631f042
Chase Southard <chase.southard@gmail.com>
2014-07-24 22:46:27 -0400

    first pass at logging

--- handlers/say_anything.rb
+++ handlers/say_anything.rb
@@ -2,7 +2,14 @@ require_relative '../lib/authentication.rb'
 class SayAnything < Sinatra::Base
   include Authentication
 
-  before '/say/' do
+  configure do
+    enable :logging
+    file = File.new(File.expand_path('log/sinatra.log'), 'a+')
+    file.sync = true
+    use Rack::CommonLogger, file
+  end
+
+  before '/say/*' do
     if authenticate(params[:token])
     else
       halt 403, haml('Access Denied')
